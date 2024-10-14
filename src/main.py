2b3c63a054e1a85e329b99c8e1a0dcd7519ce349
John Rogers <9968130+jdrbc@users.noreply.github.com>
2024-10-14 13:34:45 -0700

    scripts for development

--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ from waitress import serve
 from app import config, create_app
 
 
-def main() -> None:
-    app = create_app()
+app = create_app()
 
+
+def main() -> None:
     serve(
         app,
         host="0.0.0.0",
