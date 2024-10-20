3bf7af04b38370d12e83a4d49982f2030183b11a
John Rogers <9968130+jdrbc@users.noreply.github.com>
2024-10-20 12:16:56 -0700

    fix up dev run

--- scripts/dev_run.py
+++ scripts/dev_run.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from typing import Optional
 
@@ -5,6 +6,9 @@ from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 
 
+# pipenv run python scripts/dev_run.py
+#
+# This script will restart the server every time a file in the src directory is modified
 # workaround some bug in flask dev server causing incompatibility with SQLAlchemy
 class ChangeHandler(FileSystemEventHandler):
     def __init__(self, cmd: str) -> None:  #
@@ -13,10 +17,15 @@ class ChangeHandler(FileSystemEventHandler):
         self.start_process()
 
     def start_process(self) -> None:
+        print("start process")
         if self.process:
+            print("Terminating existing process")
             self.process.terminate()
-        with subprocess.Popen(self.cmd, shell=True) as process:
-            self.process = process
+            self.process.wait()  # Ensure the process has terminated
+        self.process = subprocess.Popen(  # pylint: disable=consider-using-with
+            self.cmd, shell=True
+        )
+        print(f"Process started with PID: {self.process.pid}")
 
     def on_any_event(self, event: FileSystemEvent) -> None:
         if "src/instance" not in event.src_path:
@@ -27,7 +36,9 @@ if __name__ == "__main__":
     command: str = "pipenv run python src/main.py"
     event_handler: ChangeHandler = ChangeHandler(command)
     observer: Observer = Observer()  # type: ignore
-    observer.schedule(event_handler, path="src", recursive=True)  # type: ignore
+    src_path = os.path.abspath("src")
+    print(f"Watching directory: {src_path}")
+    observer.schedule(event_handler, path=src_path, recursive=True)  # type: ignore
     observer.start()  # type: ignore
     try:
         while True:
