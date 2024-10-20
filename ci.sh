869fbbf6233bc48080f6848805f8fcb2537660fc
Frederick Robinson <frederick.robinson@frrad.com>
2024-10-19 21:46:46 -0700

    lint config

--- ci.sh
+++ ci.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Run mypy
 pipenv run mypy .
 
-# Run pylint
-pipenv run pylint **/*.py
+pipenv run pylint .
 
-# Run black
 pipenv run black .
 
-# Run isort
 pipenv run isort .
 
-# Run pytest
 pipenv run pytest
