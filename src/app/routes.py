0687fcab8ab6ba1cbd3d8bf4e4e28ad2c0f7f58b
Frederick Robinson <frederick.robinson@frrad.com>
2024-12-14 00:41:48 -0800

    better function name

--- src/app/routes.py
+++ src/app/routes.py
@@ -32,7 +32,7 @@ def index() -> flask.Response:
 
 
 @main_bp.route("/post/<string:p_guid>.html", methods=["GET"])
-def post(p_guid: str) -> flask.Response:
+def post_page(p_guid: str) -> flask.Response:
     post = Post.query.filter_by(guid=p_guid).first()
 
     return flask.make_response(flask.render_template("post.html", post=post), 200)
