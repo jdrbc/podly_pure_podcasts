21d50e6e80c2e4e46b85a33c0d768a84e85e5435
Frederick Robinson <frederick.robinson@frrad.com>
2025-01-04 21:12:32 -0800

    better variable names

--- src/shared/processing_paths.py
+++ src/shared/processing_paths.py
@@ -12,15 +12,21 @@ class ProcessingPaths:
     classification_dir: Path
 
 
-def paths_from_unprocessed_path(unprocessed_path: str, title: str) -> ProcessingPaths:
-    filename = Path(unprocessed_path).name
-    sanitized_title = re.sub(r"[^a-zA-Z0-9\s]", "", title)
+def paths_from_unprocessed_path(
+    unprocessed_path: str, feed_title: str
+) -> ProcessingPaths:
+    unprocessed_filename = Path(unprocessed_path).name
+    sanitized_feed_title = re.sub(r"[^a-zA-Z0-9\s]", "", feed_title)
 
-    audio_processing_dir = Path(PROCESSING_DIR) / sanitized_title / filename
+    audio_processing_dir = (
+        Path(PROCESSING_DIR) / sanitized_feed_title / unprocessed_filename
+    )
     classification_dir = audio_processing_dir / "classification"
 
     return ProcessingPaths(
-        post_processed_audio_path=Path("srv") / sanitized_title / filename,
+        post_processed_audio_path=Path("srv")
+        / sanitized_feed_title
+        / unprocessed_filename,
         audio_processing_dir=audio_processing_dir,
         classification_dir=classification_dir,
     )
