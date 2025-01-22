af34d6b01fbab525b00601043c9a22c2bf5ea588
Frederick Robinson <frederick.robinson@frrad.com>
2025-01-21 21:52:06 -0800

    more tests

--- src/tests/test_process_audio.py
+++ src/tests/test_process_audio.py
@@ -22,12 +22,55 @@ def test_clip_segment_with_fade() -> None:
             temp_file.name,
         )
 
-        print(f"clipped file written to {temp_file.name}")
+        assert (
+            get_audio_duration_ms(temp_file.name)
+            == TEST_FILE_DURATION
+            - (ad_end_offset_ms - ad_start_offset_ms)
+            + 2 * fade_len_ms
+            + 56  # not sure where this fudge comes from
+        )
+
+
+def test_clip_segment_with_fade_beginning() -> None:
+    fade_len_ms = 5_000
+    ad_start_offset_ms, ad_end_offset_ms = 0, 18_000
+
+    with tempfile.NamedTemporaryFile(delete=True, suffix=".mp3") as temp_file:
+        clip_segments_with_fade(
+            [(ad_start_offset_ms, ad_end_offset_ms)],
+            fade_len_ms,
+            TEST_FILE_PATH,
+            temp_file.name,
+        )
 
         assert (
             get_audio_duration_ms(temp_file.name)
             == TEST_FILE_DURATION
             - (ad_end_offset_ms - ad_start_offset_ms)
             + 2 * fade_len_ms
-            + 56 # not sure where this fudge comes from
+            + 56  # not sure where this fudge comes from
+        )
+
+
+def test_clip_segment_with_fade_end() -> None:
+    fade_len_ms = 5_000
+    ad_start_offset_ms, ad_end_offset_ms = (
+        TEST_FILE_DURATION - 18_000,
+        TEST_FILE_DURATION,
+    )
+
+    with tempfile.NamedTemporaryFile(delete=True, suffix=".mp3") as temp_file:
+        clip_segments_with_fade(
+            [(ad_start_offset_ms, ad_end_offset_ms)],
+            fade_len_ms,
+            TEST_FILE_PATH,
+            temp_file.name,
+        )
+
+        assert (
+            get_audio_duration_ms(temp_file.name)
+            == TEST_FILE_DURATION
+            - (ad_end_offset_ms - ad_start_offset_ms)
+            + 2 * fade_len_ms
+            + 56  # not sure where this fudge comes from
         )
