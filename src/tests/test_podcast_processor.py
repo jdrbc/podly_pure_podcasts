b8dc0692a1ffdc93e24b7e0f1d2596d45e78f4b9
Frederick Robinson <frederick.robinson@frrad.com>
2025-01-24 21:19:27 -0800

    don't import at top level

--- src/tests/test_podcast_processor.py
+++ src/tests/test_podcast_processor.py
@@ -2,13 +2,18 @@ import os
 
 import pytest
 
-from podcast_processor.podcast_processor import PodcastProcessor
 from shared.config import get_config
 
 
 @pytest.mark.skip
 def test_call_model() -> None:
     """for convenient local testing. marked skip for CI"""
+
+    # this import won't work without a valid config.
+    from podcast_processor.podcast_processor import (  # pylint: disable=import-outside-toplevel
+        PodcastProcessor,
+    )
+
     os.environ["LITELLM_LOG"] = "DEBUG"
 
     config = get_config("config/config.yml")
