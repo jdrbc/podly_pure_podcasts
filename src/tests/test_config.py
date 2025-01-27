a675d58a3e717f999928555ce6a43b81cd382c44
Frederick Robinson <frederick.robinson@frrad.com>
2025-01-27 10:14:36 -0800

    use litellm for completion requests (#60)
    * `pipenv upgrade litellm`
    
    * move over code, not tested yet
    
    * preserve old behavior
    
    * config changes
    
    * missing files
    
    * don't import at top level
    
    * rename llm model
    
    * upgrade litellm to 1.59.8
    
    * remove type ignore

--- src/tests/test_config.py
+++ src/tests/test_config.py
@@ -39,7 +39,7 @@ def test_example_config() -> None:
             min_ad_segment_length_seconds=14,
             min_confidence=0.8,
         ),
-        openai_base_url="https://api.openai.com/v1",
+        openai_base_url=None,
         whisper=LocalWhisperConfig(model="base"),
         remote_whisper=None,
         whisper_model=None,
@@ -72,7 +72,7 @@ def test_remote_whisper_example_config() -> None:
             min_ad_segment_length_seconds=14,
             min_confidence=0.8,
         ),
-        openai_base_url="https://api.openai.com/v1",
+        openai_base_url=None,
         whisper=RemoteWhisperConfig(
             model="whisper-2",
             base_url="http://localhost/v99",
@@ -86,3 +86,42 @@ def test_remote_whisper_example_config() -> None:
     )
 
     assert get_config("config/config_remote_whisper.yml.example") == expected_config
+
+
+def test_anthropic_example_config() -> None:
+    expected_config = Config(
+        openai_api_key="sk-proj-XXXXXXXXXXXXXXXXXXXXXXXX",
+        openai_base_url=None,
+        openai_max_tokens=4096,
+        openai_model="anthropic/claude-3-5-sonnet-20240620",
+        openai_timeout=300,
+        output=OutputConfig(
+            fade_ms=3000,
+            min_ad_segement_separation_seconds=60,
+            min_ad_segment_length_seconds=14,
+            min_confidence=0.8,
+        ),
+        podcasts=None,
+        processing=ProcessingConfig(
+            system_prompt_path="config/system_prompt.txt",
+            user_prompt_template_path="config/user_prompt.jinja",
+            num_segments_to_input_to_prompt=30,
+        ),
+        server=None,
+        server_port=5001,
+        background_update_interval_minute=None,
+        threads=1,
+        whisper=RemoteWhisperConfig(
+            whisper_type="remote",
+            base_url="http://localhost/v99",
+            api_key="this_is_fake",
+            language="en",
+            model="whisper-2",
+        ),
+        remote_whisper=None,
+        whisper_model=None,
+        automatically_whitelist_new_episodes=True,
+        number_of_episodes_to_whitelist_from_archive_of_new_feed=1,
+    )
+
+    assert get_config("config/config_anthropic.yml.example") == expected_config
