685a2deb2e29b95778d9f43533bfdd37ef9cb778
Frederick Robinson <frederick.robinson@frrad.com>
2025-04-18 08:53:33 -0700

    change parsing, associated tests

--- src/podcast_processor/model_output.py
+++ src/podcast_processor/model_output.py
@@ -1,20 +1,27 @@
+import json
 from typing import List
 
 from pydantic import BaseModel
 
 
 class AdSegmentPrediction(BaseModel):
-    ad_segments: List[float]
+    segment_id: float
     confidence: float
 
 
-def clean_and_parse_model_output(model_output: str) -> AdSegmentPrediction:
-    assert model_output.count("{") == 1, f"{model_output}"
-    assert model_output.count("}") == 1, f"{model_output}"
-    model_output = model_output[model_output.index("{") : 1 + model_output.index("}")]
+def clean_and_parse_model_output(model_output: str) -> List[AdSegmentPrediction]:
+    assert model_output.count("[") == 1, f"{model_output}"
+    assert model_output.count("]") == 1, f"{model_output}"
+    model_output = model_output[model_output.index("[") : 1 + model_output.index("]")]
 
     model_output = model_output.replace("'", '"')
     model_output = model_output.replace("\n", "")
     model_output = model_output.strip()
 
-    return AdSegmentPrediction.model_validate_json(model_output)
+    data = json.loads(model_output)
+
+    try:
+        return [AdSegmentPrediction(**item) for item in data]
+    except TypeError:
+        print(f"{model_output=}")
+        raise
